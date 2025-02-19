6cc416daa227005612deb3fc73e2180bcbe583c4
harshitsingh737 <152790216+harshitsingh737@users.noreply.github.com>
2025-02-19 19:12:35 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";  // treat  all JS code as newer version
+
+// alert("hello");  // we cannot write this because we are using node js and not the browser
+
+console.log(3+3);
+ 
+let name ="Harshit"
+let age = 12
+let isTrue=false
+
+
+// premitive datatype
+// number => range(2 to the power 53)
+// bigint
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+// object
+console.log(typeof "harshit")
+console.log(typeof age)
+console.log(typeof null)  // object
+console.log(typeof undefined); // undefined
+
