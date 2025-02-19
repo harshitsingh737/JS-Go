832153b193e71a4a611f0897c555e62f5b857567
harshitsingh737 <152790216+harshitsingh737@users.noreply.github.com>
2025-02-19 20:12:25 +0000

    conversion Operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,43 @@
+let score ="33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+let score1="33abc";
+console.log(score1);
+console.log(typeof score1);
+
+
+let change = Number(score1);
+
+console.log(change);
+
+let score2=true;
+let change2=Number(score2);
+console.log(change2);
+
+// "33" => 33
+// "33abc" => NaN
+// true =>1 ; false=>0;
+
+let isLoggedIn ="harshit";
+
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0=> false
+
+// "" =>false
+// "hjkda"=>true
+
+let stringNumber=33
+let stringToNum=String(stringNumber)
+console.log(stringToNum);
+console.log(typeof stringToNum);
+
+
+
