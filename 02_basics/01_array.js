bdcde38a5ecaf3286bebd8555ff776c0b777d866
harshitsingh737 <152790216+harshitsingh737@users.noreply.github.com>
2025-02-26 08:58:43 +0000

    array basics

---
+++ 02_basics/01_array.js
@@ -0,0 +1,48 @@
+// Array 
+
+const myArr=[0,1,2,3,4,5]
+const myHeros = ["powerRanger","shaktiman","Krrish"]
+
+
+// console.log(myArr[0]);
+
+const myArr2=new Array(1,2,3,4)
+
+// Array methods
+
+// myArr.push(6);
+// myArr.push(7)
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.unshift(9)
+// myArr.unshift(10)
+// myArr.shift()
+// console.log(myArr)
+
+
+
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(18))
+
+
+
+// const newArr = myArr.join()
+// console.log(myArr)
+// console.log(typeof newArr);
+// console.log(newArr)
+
+
+// slice , splice
+
+console.log("A ",myArr);
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B ",myArr);
+
+const myn2=myArr.splice(1,3);
+console.log(myn2);
+
+console.log("C ",myArr);
+
