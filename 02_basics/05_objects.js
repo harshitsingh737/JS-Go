bff77949365ba8381a76c4553d2cf7ea897f93ad
harshitsingh737 <152790216+harshitsingh737@users.noreply.github.com>
2025-02-27 18:30:39 +0000

    objects

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,12 @@
+const course ={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"Hitesh"
+}
+
+// console.log(course.courseInstructor);
+
+const {courseInstructor:instructor}=course  //object destructure
+// console.log(courseInstructor);
+console.log(instructor);
+
