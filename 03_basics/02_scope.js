6f14d0d712cb9223c08ece42a3b634b40eaff3cf
harshitsingh737 <152790216+harshitsingh737@users.noreply.github.com>
2025-03-01 11:06:23 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,14 @@
+
+let a =300
+if(true)
+{
+    let a=10
+    const b =20
+    console.log("Inner a is :",a);
+    // var c=30   // global scope
+}
+    
+
+console.log(a);
+// console.log(b);
+// console.log(c);
