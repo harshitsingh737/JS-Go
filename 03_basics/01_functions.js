35efc9eb08b21c749094fecbab1348bfcc8fbf0b
harshitsingh737 <152790216+harshitsingh737@users.noreply.github.com>
2025-03-01 09:41:26 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -44,4 +44,54 @@ console.log(loginUserMessage(""))
 // console.log(loginUserMessage())   // if user does not pass any name argument then it will show undefined, so tackle this we have to introdeuce a if statement in a function
 console.log(loginUserMessage());
 
+
+
+
+// REST OPERATOR
+// The rest operator (...) in JavaScript is used to collect multiple arguments into a single array. It allows functions to accept an indefinite number of arguments and bundle them into an array.
+// Usage of the Rest Operator
+// The rest operator is represented by three dots (...) and is primarily used in function parameters.
+function calculateCartPrice(...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(2,3,5));
+
+
+const [first, second, ...rest] = [10, 20, 30, 40, 50];
+console.log(first);  // Output: 10
+console.log(second); // Output: 20
+console.log(rest);   // Output: [30, 40, 50]
+
+
+
+// Using object in function
+const user = {
+    username :"harshit",
+    price:199
+}
+function handleObject(anyObject){
+    console.log(`UserName is ${anyObject.username} and price is ${anyObject.price}`);
+}
+handleObject(user)
+
+// handleObject({
+//     username:"sam",
+//     price:399
+// })
+
+// passing array in function
+const myArray=[20,30,40,50]
+function solve(getArray)
+{
+    return getArray[1]
+}
+
+console.log(solve(myArray))
+
+
+
+
+ 
+
     
