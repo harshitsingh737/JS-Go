45ea10767d8e959a57a19f50aedb66085e37b0cf
harshitsingh737 <152790216+harshitsingh737@users.noreply.github.com>
2025-03-04 15:45:18 +0000

    ifelse

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -42,3 +42,30 @@ if(score > 100){
     console.log(`User power : ${power}`);
 }
 
+const balance =1000
+// if(balance >500 ) console.log("test");
+
+if(balance <500)
+{
+    console.log("less than 500");
+}
+else if(balance < 750)
+{
+    console.log("less than 750");
+}
+else{
+    console.log("less than 1200");
+}
+
+const userLoggedIn=true 
+const debitCard=true
+
+if(userLoggedIn && debitCard)
+{
+    console.log("Allow to buy course");
+}    
+
+    
+        
+    
+    
