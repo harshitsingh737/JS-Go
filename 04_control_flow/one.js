6d8d66c22c37c69e0f766dd09fb19875e1efc07d
harshitsingh737 <152790216+harshitsingh737@users.noreply.github.com>
2025-03-04 14:32:15 +0000

    ifelse

---
+++ 04_control_flow/one.js
@@ -0,0 +1,44 @@
+// if
+const loggedIn = true;
+if (loggedIn) {
+
+}
+if (3 != 2) {
+
+}
+
+if(2=="2"){
+    console.log("executed");
+}
+    
+if(2==="2"){
+    console.log("executed");
+}
+
+if(2!=="2"){
+    console.log("executed");
+}
+
+if(2!="2"){
+    console.log("executed");
+}
+
+const temperature = 41
+
+if(temperature ===41)
+{
+    console.log("Less than 50");
+}
+ 
+else{
+    console.log("Temperature is greater than 50");
+}
+ 
+// < , > , <= , >= , == , != , === , !==
+
+const score =200
+if(score > 100){
+    const power="fly"
+    console.log(`User power : ${power}`);
+}
+
