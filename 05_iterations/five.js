3f60c32b35c22356e1ee3829a839fb9bfc6a1e28
harshitsingh737 <152790216+harshitsingh737@users.noreply.github.com>
2025-03-05 17:50:26 +0000

    foreach

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -2,7 +2,43 @@ const coding =["js","ruby","java","python","cpp"]
 
 
 // callback function does not have name so remove name from function
-coding.forEach( function (val){
-    console.log(val);
+// coding.forEach( function (val){
+//     console.log(val);
+// } )
     
+// arrow function
+// coding.forEach((item) => {
+//     console.log(item);
+// } )
+
+
+// function printMe(item){
+//     console.log(item);
+// }
+// coding.forEach(printMe)
+
+
+// coding.forEach( (item,index,arr) => {
+//     console.log(item,index,arr);
+// } )
+    
+const myCoding = [
+    {
+        languageName:"javaScript",
+        languageFilename:"js"
+    },
+
+    {
+        languageName:"java",
+        languageFilename:"java"
+    },
+
+    {
+        languageName:"python",
+        languageFilename:"py"
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName);   
 } )
