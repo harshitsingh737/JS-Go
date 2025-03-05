d8a2cc83fd7ec95afc62c2d8dafb3938bd5ea389
harshitsingh737 <152790216+harshitsingh737@users.noreply.github.com>
2025-03-05 06:49:06 +0000

    loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,14 @@
+// for loop
+
+for (let index = 0; index < 10; index++) {
+    const element = index;
+
+    if(index==5)
+    {
+        console.log("5 is the best number");
+        
+    }
+    console.log(element);
+    
+}
+     
