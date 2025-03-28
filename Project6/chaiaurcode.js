4efdf5d37a7be55e9d497f5ea8030ea33e95c5f1
harshitsingh737 <152790216+harshitsingh737@users.noreply.github.com>
2025-03-28 18:10:41 +0000

    project6

--- Project6/chaiaurcode.js
+++ Project6/chaiaurcode.js
@@ -2,13 +2,31 @@
 
 const randomColor = function()
 {
-const hex = '0123456789ABCDEF'
+const hex = '0123456789ABCDEF';
 let color='#';
 for(let i=0;i<6;i++)
 {
-    color+=hex[Math.floor(Math.random()*16)]
+    color+=hex[Math.floor(Math.random () * 16)];
 }
 return color;
 };
 
-console.log(randomColor());
+// console.log(randomColor());
+
+let stopChange;
+const startChangingColor = function(){
+    
+    stopChange=setInterval(changeBgColor,1000)
+    function changeBgColor(){
+        document.body.style.backgroundColor = randomColor();
+    }
+
+}
+
+const stopChangingColor = function(){
+    clearInterval(stopChange)
+    stopChange = null;
+};
+
+document.querySelector('#start').addEventListener('click' , startChangingColor);
+document.querySelector('#stop').addEventListener('click' ,stopChangingColor);
