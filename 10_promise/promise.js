44cb0eac7f6e83deea1f993e1a27efc3dd75c6ec
harshitsingh737 <152790216+harshitsingh737@users.noreply.github.com>
2025-03-31 21:45:47 +0000

    promise

--- 10_promise/promise.js
+++ 10_promise/promise.js
@@ -3,6 +3,8 @@ const promiseOne = new Promise(function(resolve,reject){
     // DB calls,cryptofraphy,network
     setTimeout(function(){
         console.log('Async task is complete');
+        // to connect resolve with .then ,i have to write resolve()
+        resolve()
     },1000,)
 })
 
@@ -10,5 +12,91 @@ const promiseOne = new Promise(function(resolve,reject){
 promiseOne.then(function(){
     console.log('Promise consumed');
 })
+     
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve();
+    },1000)
+}).then(function(){
+    console.log("Async 2 resolved");
+})
+
+const promiseThree = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        // We can pass the parameters in the resolve()
+        resolve({username:"Chai",email:"chai@example.com"})
+    },1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = false
+        if(!error){
+            resolve({username:"hitesh" , password:"123"})
+        } else{
+            reject('ERROR:Something went wrong')
+        }
+    },1000)
+})
+
+// Chaining of .then() . in this the value returned by first then goes to second .then()
+ promiseFour.then((user) =>{
+    console.log(user);
+    return user.username
+}).then((username) =>{
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
+}).finally(()=>{
+    console.log("The promise is either resolve or rejected");
+})
+
+const promiseFive = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = false
+        if(!error){
+            resolve({username:"javaScript" , password:"123"})
+        } else{
+            reject('ERROR:JS went wrong')
+        }
+    },1000)
+})
+
+// Promise ek object hai so aap isko promiseFive() aise consume nhi karte ho , use promiseFive 
+
+// Another way for .then()
+// problem of async is that it does not handle error directly so we need try catch block
+async function consumePromiseFive(){
+    // const response = await promiseFive
+    // console.log(response);
+    try{
+        const response = await promiseFive
+        console.log(response);
+    }
+
+    catch(error){
+        console.log(error);
+    }
+}
+
+consumePromiseFive()
+
+async function getAllUsers(){
+  const response = await fetch('')
+}
+    
+    
+    
+    
+
+
+
+    
+    
     
         
