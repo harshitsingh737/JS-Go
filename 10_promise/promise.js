14e8e810b5633ab14d80b847fe138048bf847c13
harshitsingh737 <152790216+harshitsingh737@users.noreply.github.com>
2025-04-02 18:17:01 +0000

    promise

--- 10_promise/promise.js
+++ 10_promise/promise.js
@@ -25,7 +25,7 @@ new Promise(function(resolve,reject){
 const promiseThree = new Promise(function(resolve,reject){
     setTimeout(function(){
         // We can pass the parameters in the resolve()
-        resolve({username:"Chai",email:"chai@example.com"})
+        resolve({username:"Chai",email:"chai@example.com"}) 
     },1000)
 })
 
@@ -78,18 +78,34 @@ async function consumePromiseFive(){
         const response = await promiseFive
         console.log(response);
     }
-
     catch(error){
         console.log(error);
     }
 }
 
+
 consumePromiseFive()
 
-async function getAllUsers(){
-  const response = await fetch('')
-}
-    
+// async function getAllUsers(){
+//   try{
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const data = await response.json()
+//     console.log(data);
+//   }
+//   catch(error){
+//         console.log("E:",error);
+//     }
+// }
+         
+// getAllUsers()
+  
+fetch('https://jsonplaceholder.typicode.com/users').then(function(response){
+    return response.json()
+}).then(function(data){
+    console.log(data);
+}).catch((error)=>console.log(error));
+
+ 
     
     
     
